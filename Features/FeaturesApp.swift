d01f249cc4faa150bf35cb2c758f2ebe26802a3d
Mladen Mikic <m.mikic@rooom.com>
2023-05-14 09:24:32 +0200

    Initial Commit

---
+++ Features/FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturesApp.swift
+//  Features
+//
+//  Created by Mladen Mikic on 14.05.2023..
+//
+
+import SwiftUI
+
+@main
+struct FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
